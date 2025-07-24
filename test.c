a14974a08ba6734663dfa1f52b5f5eca5e01a2e1
Raidboy <132174647+0xB0y426@users.noreply.github.com>
2025-07-24 05:44:01 -0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "inc/alloc64.h"
+
+int main() {
+    int *a = (int *)cflloc(sizeof(int));
+
+    if(a) {
+        *a = 64;
+    }
+
+    printf("Memory Allocated %d bytes.\n", *a);
+
+    frdom();
+
+    return 0;
+}
+
